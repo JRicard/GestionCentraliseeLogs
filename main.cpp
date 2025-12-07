084e21e43f3cd3522ca16226d9ea927b15cead9a
JRicard <juliette.ricard@lycee-de-beauvoir.education>
2025-12-07 16:07:08 +0100

    Création main.h et documentation initiale

--- main.cpp
+++ main.cpp
@@ -1,9 +1,15 @@
-#include <iostream>
+/**
+* @file main.cpp
+* @brief Fichier principal qui permet le lancement du programme via la fonction main()
+* @author J. Ricard
+* @version v1.0
+* @date 07/12/2025
+*/
 
-using namespace std;
+#include "main.h"
 
 int main(){
-    cout << "Modele de projet" << endl;
+    cout << "Gestion centralisee des logs" << endl;
     return 0;
 }
 
--- readme.md
+++ readme.md
@@ -1,14 +1,5 @@
-## Modèle de projet pour le C++
+## Gestion centralisée des logs
 
-Ce dossier contient les fichiers pour initier un projet C++ avec Cmake.
-
-Il contient également les fichiers pour l'intégration continue avec la forge logicielle Gitlab.
-
-## Que faire ?
-
-* Télécharger ce dossier
-* Ouvrir le projet avec CLion
-* Initialiser un dépot Git
 
 
 
